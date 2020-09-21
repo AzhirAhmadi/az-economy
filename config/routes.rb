e174dc214461896b4d249c9172fa4c04d67364ae
Azhir <azhir@buzzjective.com>
2020-09-21 09:55:56 +0330

    add device to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,38 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  # namespace :admin do
+  #   resources :users
+  # end
+
+  # namespace :api do
+  #   namespace :v1 do
+  #     namespace :teacher do
+  #       resources :users do
+  #         resources :environments, only: %i[index show] do
+  #           resources :field_rent_datas, only: %i[index show]
+  #           resources :loan_datas, only: %i[index show]
+  #           resources :framing_datas, only: %i[index show]
+  #           resources :product_datas, only: %i[index show]
+            
+  #           resources :companies, only: %i[show new create edit update]
+  #         end
+  #       end
+  #     end
+    
+  #     namespace :student do
+  #       resources :users do
+  #         resources :environments, only: %i[index show] do
+  #           resources :field_rent_datas, only: %i[index show]
+  #           resources :loan_datas, only: %i[index show]
+  #           resources :framing_datas, only: %i[index show]
+  #           resources :product_datas, only: %i[index show]
+    
+  #           resources :companies, only: %i[show new create edit update]
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
 end
