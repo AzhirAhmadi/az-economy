6ff13d516e99f318c8577b5043c591dedb44e1d2
Azhir <azhir@buzzjective.com>
2020-09-22 12:54:43 +0330

    add seed for users and roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+general_params = {
+    first_name: 'Azhir',
+    last_name: 'Ahmadi',
+    password: '123456'
+}
+User.create(general_params.merge({
+    email: 'admin@test.com',
+    role: Admin.create(),
+}))
+User.create(general_params.merge({
+    email: 'teacher@test.com',
+    role: Teacher.create(),
+}))
+User.create(general_params.merge({
+    email: 'student@test.com',
+    role: Student.create(student_number: 123456),
+}))
